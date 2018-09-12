d42d61883190c4bc67016bc46253f6f7cfe66532
Stoian Ivanov <s.ivanov@teracomm.bg>
2018-09-12 12:17:04 +0300

    samples; more complete simpledump.js

--- simpledump.js
+++ simpledump.js
@@ -3,26 +3,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const listener_1 = require("./listener");
 const coap_encode_1 = require("./coap_encode");
 let l = new listener_1.Listener();
-let first = true;
+let stat_from = new Set();
+let desc_from = new Set();
 l.on('/cit/s', (m) => {
     if (m.pl != undefined) {
-        console.log("CoIoT status from " + m.coiot_devidrev + '@' + m.rinfo.address + ':' + m.pl.toString('utf8'));
-        if (first && m.coiot_dev == 'SHSW-44') {
-            first = false;
-            l.send_and_track(coap_encode_1.coap_prepare_get("/cit/d"), m.rinfo, (resp, req) => {
-                if (resp == undefined) {
-                    console.log("Timeout for reply from " + m.coiot_devidrev + '@' + m.rinfo.address);
+        if (m.coiot_dev == undefined)
+            return;
+        let statadded;
+        if ((statadded = stat_from.has(m.coiot_dev)) && desc_from.has(m.coiot_dev))
+            return;
+        if (!statadded) {
+            stat_from.add(m.coiot_dev);
+            console.log("CoIoT status from " + m.coiot_devidrev + '@' + m.rinfo.address + ':' + m.pl.toString('utf8'));
+        }
+        l.send_and_track(coap_encode_1.coap_prepare_get("/cit/d"), m.rinfo, (resp, req) => {
+            if (m.coiot_dev == undefined || desc_from.has(m.coiot_dev))
+                return;
+            if (resp == undefined) {
+                console.log("Timeout for reply from " + m.coiot_devidrev + '@' + m.rinfo.address);
+            }
+            else {
+                if (resp.pl == undefined) {
+                    console.log("CaAP reply from " + m.coiot_devidrev + '@' + m.rinfo.address + ':', resp);
                 }
                 else {
-                    if (resp.pl == undefined) {
-                        console.log("CaAP reply from " + m.coiot_devidrev + '@' + m.rinfo.address + ':', resp);
-                    }
-                    else {
-                        console.log("CoIoT description from " + m.coiot_devidrev + '@' + m.rinfo.address + ':' + resp.pl.toString('utf8'));
-                    }
+                    desc_from.add(m.coiot_dev);
+                    console.log("CoIoT description from " + m.coiot_devidrev + '@' + m.rinfo.address + ':' + resp.pl.toString('utf8'));
                 }
-            });
-        }
+            }
+        });
     }
     else {
         console.log("CoIoT status from " + m.coiot_devidrev + '@' + m.rinfo.address + ' with no PL?!');
--- ts/simpledump.ts
+++ ts/simpledump.ts
@@ -2,25 +2,34 @@ import {Listener} from "./listener"
 import { Coap_Message } from "./coap_message";
 import { coap_prepare_get } from "./coap_encode";
 let l:Listener=new Listener();
-let first=true;
+
+let stat_from=new Set<string>();
+let desc_from=new Set<string>();
+
 l.on('/cit/s',(m:Coap_Message)=>{
 	if (m.pl!=undefined) {
-		console.log("CoIoT status from "+m.coiot_devidrev+'@'+m.rinfo.address+':'+m.pl.toString('utf8'));
-		if (first && m.coiot_dev=='SHSW-44'){
-			first=false;
-			l.send_and_track(coap_prepare_get("/cit/d"),m.rinfo,(resp,req)=>{
-				if (resp==undefined) {
-					console.log("Timeout for reply from "+m.coiot_devidrev+'@'+m.rinfo.address);
-				} else {
-					if (resp.pl==undefined) {
-						console.log("CaAP reply from "+m.coiot_devidrev+'@'+m.rinfo.address+':',resp);
-					} else {
-						console.log("CoIoT description from "+m.coiot_devidrev+'@'+m.rinfo.address+':'+resp.pl.toString('utf8'));
-					}
-				}
-			});
-			
+		if (m.coiot_dev==undefined) return;
+		let statadded:boolean;
+		if ((statadded=stat_from.has(m.coiot_dev)) && desc_from.has(m.coiot_dev)) return;
+		if (!statadded) {
+			stat_from.add(m.coiot_dev);
+			console.log("CoIoT status from "+m.coiot_devidrev+'@'+m.rinfo.address+':'+m.pl.toString('utf8'));
 		}
+
+		l.send_and_track(coap_prepare_get("/cit/d"),m.rinfo,(resp,req)=>{
+			if (m.coiot_dev==undefined || desc_from.has(m.coiot_dev) ) return;
+			if (resp==undefined) {
+				console.log("Timeout for reply from "+m.coiot_devidrev+'@'+m.rinfo.address);
+			} else {
+				if (resp.pl==undefined) {
+					console.log("CaAP reply from "+m.coiot_devidrev+'@'+m.rinfo.address+':',resp);
+				} else {
+					desc_from.add(m.coiot_dev);
+					console.log("CoIoT description from "+m.coiot_devidrev+'@'+m.rinfo.address+':'+resp.pl.toString('utf8'));
+				}
+			}
+		});
+			
 	} else {
 		console.log("CoIoT status from "+m.coiot_devidrev+'@'+m.rinfo.address+' with no PL?!');
 	}
